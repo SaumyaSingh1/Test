82947b05be2e195036db98c0dcac6b18f3ff7b8c
SaumyaSingh1 <115228860+SaumyaSingh1@users.noreply.github.com>
2022-10-09 18:48:05 +0530

    Add files via upload

---
+++ recursion.cpp
@@ -0,0 +1,66 @@
+ //sum of no. upto n
+#include<iostream>
+ using namespace std;
+/* 
+ int sum(int n){
+if(n==0){
+    return 0;
+} 
+int prev = sum(n-1);
+return  n + prev;
+ }
+ int main(){
+   int n;
+    cin>>n;
+cout<<sum(n)<<endl;
+return 0;
+ } */
+ //POWER 
+ /*
+int power(int n,int p){
+if (p==0){
+    return 1;}
+int prevpower = power(n, p-1);
+return n*prevpower;
+}
+int main(){
+    //int n,p;
+    //cin>>n>>p;
+    cout<<power(2,3)<<endl;
+    return 0;
+}*/
+/*int factorial(int n){
+    if(n==0){
+        return 1;
+    }
+    int prevfac=factorial(n-1);
+    return n*prevfac;
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<factorial(n)<<endl;
+}
+*/
+//increasing decreasing 
+/*
+void dec(int n){
+if(n==0){
+    return;
+} 
+cout<<n<<endl;
+dec(n-1);}
+void inc(int n){
+    if(n==0){
+        return;
+    } inc(n-1);
+    cout<<n<<endl;}
+int main()
+{
+    int n;
+    cin>>n;
+    dec(n);
+    inc(n);
+}*/
+int  
+
